adbf2f6d20fb3506366034ee4101569074ea9b2b
Vboer <mischukav@gmail.com>
2020-02-10 09:56:01 +0200

    Add files via upload

---
+++ scriptOO.js
@@ -0,0 +1,49 @@
+document.getElementById("submit").addEventListener("click", getTimeRow);
+
+function getTimeRow (){
+    const datesObj = new dates (document.getElementById("from").value, document.getElementById("to").value,).getDates().datesToString().getPrices();
+       console.log(datesObj);
+
+}
+
+function dates (from, to){
+    this.from = from;
+    this.to = to;
+    this.dateArray = [];
+    this.priceArray = [];
+};
+dates.prototype.getDates = function (){
+    
+    let fromDate = new Date(this.from);
+    const toDate = new Date(this.to);
+    if (fromDate || toDate > Date() ){
+        alert('Dates can\'t be in future');
+        return ;
+    } else {
+        while (fromDate <= toDate){
+            if(fromDate.getDay() > 0 && fromDate.getDay() < 6 ){
+                this.dateArray.push(new Date(fromDate));
+            }
+        fromDate.setDate(fromDate.getDate() + 1);
+        
+        }
+    }
+    return this;
+}
+dates.prototype.datesToString = function(){
+    for (let i = 0; i < this.dateArray.length; i++) {
+        this.dateArray[i] = this.dateArray[i].toISOString().split('T')[0].replace(/-/g,'');
+    }
+    return this;
+}
+dates.prototype.getPrices = function(){
+    this.dateArray.forEach(element => {
+        const url =`https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=USD&date=${element}&json`;
+      fetch(url)
+            .then(res => res.json())
+            .then(output => {
+                this.priceArray.push(output[0].rate);
+                });
+        });
+    return this;
+}
